a9349427759aa2c555eef9eca414671f02754d49
Jude Wafa <jood237@yahoo.com>
2025-10-15 23:37:45 +0300

    Modify homepage url

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-   path('homepage/',views.homepage, name='homepage'),
+   path('',views.homepage, name='homepage'),
    path('pipelines/create/', views.create_pipeline, name='create_pipeline'),
    path('pipelines/<int:pipeline_id>/', views.pipeline_detail, name='pipeline_detail'),
    path('move_item/', views.move_item, name='move_item'),
